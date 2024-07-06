867bb3fc4995cdcb98de1ed05d06658ecae9b1b6
Aelson <aelson.carvalho98@outlook.com>
2024-07-06 12:03:06 -0300

    acidionando testes

--- src/test/java/com/aelson/todolist/services/FuncionarioServiceTests.java
+++ src/test/java/com/aelson/todolist/services/FuncionarioServiceTests.java
@@ -1,6 +1,7 @@
 package com.aelson.todolist.services;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -19,6 +20,11 @@ public class FuncionarioServiceTests {
 
     @BeforeEach
     public void setup(){
+
+    }
+
+    @Test
+    public void deve_retornar_um_funcionarioResponse_ao_criar_um_novo_funcionario(){
 
     }
 
